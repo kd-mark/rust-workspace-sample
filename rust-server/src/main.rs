bccb7f93fd22e19f88437bd9b491713f7a0be7b0
Marco Kuidja <marco.kuidja@adorsys.com>
2025-03-21 10:53:17 +0100

    feat(server): exposed compressed endpoint

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -4,7 +4,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use handlers::upload_file;
+use handlers::{compress_file, upload_file};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -12,7 +12,8 @@ async fn main() {
     // Create a new Axum router
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/upload-files", post(upload_file::upload_files));
+        .route("/upload-files", post(upload_file::upload_files))
+        .route("/compress-files", post(compress_file::compress_all_files));
 
     // Define the address for the server to listen on
     let listener = match TcpListener::bind("0.0.0.0:3000").await {
