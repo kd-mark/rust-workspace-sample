b8591a0a1877349fc6cb83adfac3ef0926401622
Marco Kuidja <marco.kuidja@adorsys.com>
2025-03-23 00:26:44 +0100

    feat(server): example path & query parameters, example middleware

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,12 +1,14 @@
 mod handlers;
-
 use axum::{
+    extract::{Path, Query},
+    response::IntoResponse,
     routing::{get, post},
     Router,
 };
 use handlers::{compress_file, upload_file};
+use serde::Deserialize;
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::{services::ServeDir, trace::TraceLayer};
 
 #[tokio::main]
 async fn main() {
@@ -21,24 +23,39 @@ async fn main() {
     // Create a new Axum router
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .route("/path-examples/:parameter", get(path_example_handler))
         .nest("/uploader", uploads)
         .nest("/compressor", compressed)
-        .fallback(|| async { r#"{"status":404,"message":"Resource Not Found"}"# });
+        .fallback(|| async { r#"{"status":404,"message":"Resource Not Found"}"# })
+        .layer(TraceLayer::new_for_http());
 
     // Define the address for the server to listen on
-    let listener = match TcpListener::bind("0.0.0.0:3000").await {
+    let ip_addr = "0.0.0.0:3000";
+    let listener = match TcpListener::bind(ip_addr).await {
         Ok(listener) => listener,
         Err(e) => {
             eprintln!("Failed to bind TcpListener to server {e}");
             return;
         }
     };
+    println!("[INFO]: Application is running http://{ip_addr}");
 
     // Start the server
-    match axum::serve(listener, app).await {
-        Ok(_) => {}
-        Err(e) => {
-            eprintln!("{e}")
-        }
-    };
+    if let Err(e) = axum::serve(listener, app).await {
+        eprintln!("{e}")
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct MyQuery {
+    gis: Option<bool>,
+    number: usize,
+}
+
+async fn path_example_handler(
+    Path(parameter): Path<String>,
+    Query(query): Query<MyQuery>,
+) -> impl IntoResponse {
+    format!("The parameter value is {parameter} and query value is {query:?}")
 }
