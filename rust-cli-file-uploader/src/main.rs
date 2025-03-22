3a8449d1dd403e97ce172b800b8cdf9f0b89e80a
Marco Kuidja <marco.kuidja@adorsys.com>
2025-03-23 00:22:12 +0100

    fix(cli): collected server response from file uploader

--- rust-cli-file-uploader/src/main.rs
+++ rust-cli-file-uploader/src/main.rs
@@ -1,6 +1,11 @@
-use reqwest::Client;
 use reqwest::multipart;
-use std::{env, fs::File, io::{self, Read}, path::Path};
+use reqwest::Client;
+use std::{
+    env,
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
 
 async fn read_file(file_path: &str) -> io::Result<(String, Vec<u8>)> {
     let file_name = Path::new(file_path)
@@ -8,30 +13,35 @@ async fn read_file(file_path: &str) -> io::Result<(String, Vec<u8>)> {
         .and_then(|name| name.to_str())
         .map(String::from)
         .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file name"))?;
-    
+
     let mut buffer = Vec::new();
     File::open(file_path)?.read_to_end(&mut buffer)?;
-    
+
     Ok((file_name, buffer))
 }
 
-async fn upload_file(client: &Client, url: &str, file_name: String, file_data: Vec<u8>) -> Result<(), reqwest::Error> {
+async fn upload_file(
+    client: &Client,
+    url: &str,
+    file_name: String,
+    file_data: Vec<u8>,
+) -> Result<String, reqwest::Error> {
     let part = multipart::Part::bytes(file_data)
         .file_name(file_name.clone())
         .mime_str("application/octet-stream")
         .unwrap_or_else(|_| panic!("Failed to set MIME type")); // Should not fail in practice
-    
+
     let form = multipart::Form::new().part("file", part);
-    
+
     let response = client.post(url).multipart(form).send().await?;
-    
+
     if response.status().is_success() {
         println!("File '{}' uploaded successfully", file_name);
+        let reponse_body = response.text().await?;
+        Ok(reponse_body)
     } else {
-        eprintln!("Failed to upload file: {:?}", response.status());
+        panic!("Failed to upload file: {:?}", response.status());
     }
-    
-    Ok(())
 }
 
 #[tokio::main]
@@ -41,14 +51,17 @@ async fn main() {
         eprintln!("Usage: {} <file_path>", args[0]);
         return;
     }
-    
+
     let file_path = &args[1];
     match read_file(file_path).await {
         Ok((file_name, file_data)) => {
             let client = Client::new();
-            let url = "http://localhost:3000/upload-files";
-            if let Err(err) = upload_file(&client, url, file_name, file_data).await {
-                eprintln!("Error uploading file: {}", err);
+            let url = "http://localhost:3000/uploader/upload";
+            match upload_file(&client, url, file_name, file_data).await {
+                Ok(data) => println!("{data}"),
+                Err(err) => {
+                    eprintln!("Error uploading file: {}", err);
+                }
             }
         }
         Err(err) => eprintln!("Error reading file: {}", err),
